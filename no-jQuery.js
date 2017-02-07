aaaf7b1d9182ed1252d2e6a5e369d5f8bab10075
tangyangyang <tangyang22007@126.com>
2017-02-07 13:58:51 +0800

    dd

--- no-jQuery.js
+++ no-jQuery.js
@@ -1,7 +1,8 @@
-//Query 
+//使用原生js替代jQuery
+//选择器
 document.querySelectorAll('selector');
 document.getElementsByClassName('classname');
 document.getElementById('id');
 
-//get Attribute
+//获得元素属性
 e.getAttribute('attr');
