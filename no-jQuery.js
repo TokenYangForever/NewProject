d62ece3663ee99417625f54670b21a8fc981577f
tangyangyang <tangyang22007@126.com>
2017-02-07 14:39:33 +0800

    update

--- no-jQuery.js
+++ no-jQuery.js
@@ -6,3 +6,24 @@ document.getElementById('id');
 
 //获得元素属性
 e.getAttribute('attr');
+
+//获取 date-**属性
+e.getAttribute('date-**');
+//支持dateset的情况下
+e.dateset["**"];
+
+//获取兄弟元素 先将el.parentNode.children的htmlcollection类型转换为数组
+function getSibings (el){
+  return Array.prototype.slice.call(el.parentNode.children,0).filter(function(i){return i!== el;});
+}
+//简写版
+function getSibings2(el){
+   return  [].filter.call(el.parentNode.children, function(child) {
+              return child !== el;
+            });
+}
+
+//获取下一个元素
+el.nextElementSibling;
+//获取上一个元素
+el.previousElementSibling;
