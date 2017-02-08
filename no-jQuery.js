4112421dd30e5b1891a9bc10140cfbcf3cdc7214
tangyangyang <tangyang22007@126.com>
2017-02-08 11:35:09 +0800

    update

--- no-jQuery.js
+++ no-jQuery.js
@@ -27,3 +27,14 @@ function getSibings2(el){
 el.nextElementSibling;
 //获取上一个元素
 el.previousElementSibling;
+//获取输入框的值
+document.querySelector('#my-input').value;
+
+//获取css样式
+// 注意：此处为了解决当 style 值为 auto 时，返回 auto 的问题（不直接使用window对象）
+const win = el.ownerDocument.defaultView;
+// null 的意思是不返回伪类元素
+win.getComputedStyle(el, null).color;
+// 设置样式.style可读可写 .getComputedStyle只可读；
+el.style.color = '#ff0011';
+
