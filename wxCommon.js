4b7940da8e26aa8ce02c339d37cdc407104a81a0
tangyangyang <tangyang22007@126.com>
2017-02-13 11:36:16 +0800

    update

--- wxCommon.js
+++ wxCommon.js
@@ -246,3 +246,23 @@ function setTitle(title) {
                 }).appendTo($('body'));
             }
 }
+//根据滚动更新进度条
+(function() {
+      var $w = $(window);
+      var $prog2 = $('.progress-indicator-2');
+      var wh = $w.height();
+      var h = $('body').height();
+      var sHeight = h - wh;
+      $w.on('scroll', function() {
+        window.requestAnimationFrame(function(){
+          var perc = Math.max(0, Math.min(1, $w.scrollTop() / sHeight));
+          updateProgress(perc);
+        });
+      });
+
+      function updateProgress(perc) {
+        $prog2.css({width: perc * 100 + '%'});
+        ditto.save_progress && store.set('page-progress', perc);
+      }
+
+    }());
