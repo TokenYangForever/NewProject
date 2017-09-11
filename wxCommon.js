45a2bab0cb687bdaa50f6e0175c3ed69327d1a34
tangyangyang <tangyang22007@126.com>
2017-09-11 22:48:49 +0800

    Update wxCommon.js

--- wxCommon.js
+++ wxCommon.js
@@ -1,125 +1,3 @@
-Zepto.cookie = function (h, m, j) {
-    if (typeof m != "undefined") {
-        j = j || {};
-        if (m === null) {
-            m = "";
-            j.expires = -1
-        }
-        var f = "";
-        if (j.expires && (typeof j.expires == "number" || j.expires.toUTCString)) {
-            var d;
-            if (typeof j.expires == "number") {
-                d = new Date();
-                d.setTime(d.getTime() + (j.expires * 24 * 60 * 60 * 1000))
-            } else {
-                d = j.expires
-            }
-            f = "; expires=" + d.toUTCString()
-        }
-        var k = j.path ? "; path=" + j.path : "";
-        var e = j.domain ? "; domain=" + j.domain : "";
-        var l = j.secure ? "; secure" : "";
-        document.cookie = [h, "=", encodeURIComponent(m), f, k, e, l].join("")
-    } else {
-        var c = null;
-        if (document.cookie && document.cookie != "") {
-            var b = document.cookie.split(";");
-            for (var g = 0; g < b.length; g++) {
-                var a = Zepto.trim(b[g]);
-                if (a.substring(0, h.length + 1) == (h + "=")) {
-                    c = decodeURIComponent(a.substring(h.length + 1));
-                    break
-                }
-            }
-        }
-        return c
-    }
-};
-/**
- * storage操作
- * SetStorage方法：设置存储storage的key和value值，如果storage存储失败，则将数据存储在cookie中
- * GetStorage方法：根据key值获取storage相对应的value值，获取失败则从cookie中获取，
- */
-var StorageHelp = {
-    SetStorage: function (e, g) {
-        if (window.localStorage) {
-            var h = window.localStorage;
-            try {
-                h.setItem(e, g)
-            } catch (f) {
-                $.cookie(e, g, { expires: 30 })
-            }
-        } else {
-            $.cookie(e, g, { expires: 30 })
-        }
-    }, 
-    GetStorage: function (d) {
-        if (window.localStorage) {
-            var f = window.localStorage;
-            try {
-                f = f.getItem(d);
-                if (f == null || f == "") {
-                    return $.cookie(d) == null ? "" : $.cookie(d)
-                } else {
-                    return f
-                }
-            } catch (e) {
-                return $.cookie(d) == null ? "" : $.cookie(d)
-            }
-        } else {
-            return $.cookie(d) == null ? "" : $.cookie(d)
-        }
-    },
-    SetSessionStorage: function(e,g){
-        if (window.sessionStorage) {
-            var h = window.sessionStorage;
-            try {
-                h.setItem(e, g)
-            } catch (f) {
-                $.cookie(e, g, { expires: 30 })
-            }
-        } else {
-            $.cookie(e, g, { expires: 30 })
-        }
-    },
-    GetSessionStorage: function(d){
-        if (window.sessionStorage) {
-            var f = window.sessionStorage;
-            try {
-                f = f.getItem(d);
-                if (f == null || f == "") {
-                    return $.cookie(d) == null ? "" : $.cookie(d)
-                } else {
-                    return f
-                }
-            } catch (e) {
-                return $.cookie(d) == null ? "" : $.cookie(d)
-            }
-        } else {
-            return $.cookie(d) == null ? "" : $.cookie(d)
-        }
-    },
-    ClearSessionStorage:function(arr){
-        if(!(arr instanceof Array)){
-            arr = ["defaultArrToClear"];
-        }
-        if (window.sessionStorage) {
-            try{
-                for(var i=0;i<arr.length;i++){
-                    sessionStorage.removeItem(arr[i]);
-                }
-            }catch(e){
-                for(var i=0;i<arr.length;i++){
-                    $.cookie(arr[i], '', {expires:-1});
-                }
-            }
-        }else{
-            for(var i=0;i<arr.length;i++){
-                $.cookie(arr[i], '', {expires:-1});
-            }
-        }
-    }
-};
 function stopScroll() {
     $("body").on("touchmove", function (a) {
         a.preventDefault();
@@ -220,8 +98,15 @@ var browser = {
         return this.replace(new RegExp(s1, "g"), s2)  
     }  
 })(String);
-// 计算两个时间戳之间的时间差返回相差的天数，小时数，分钟数，秒数
+  /**
+   * 计算两个日期时间的时间差
+   * @param {Date, Date}  date1 date2
+   * @return {object | null} 
+   * @example
+   * getDiff(new Date('2017-09-08'), new Date())
+   */
 function getDiff(date1, date2) {
+     if (!date1.getTime || !date2.getTime) return null
      var ms = (date1.getTime() - date2.getTime());
      var day1 = Math.round(ms / 24 / 3600 / 1000),
          hh1 = Math.round((ms / 3600 / 1000) % 24),
@@ -266,6 +151,13 @@ function setTitle(title) {
       }
 
     }());
+  /**
+   * 将canvas转化为image格式
+   * @param {string}  cId
+   * @return {object HTMLImageElement} 
+   * @example
+   * canvasToImg('canvas')  canvasToImg('#canvarsId')
+   */
 function canvasToImg(cId){
     let canvas = document.querySelector(cId)
     if (!canvas || !canvas.toDataURL) return new Image()
@@ -300,119 +192,6 @@ Date.getWeek = function(a) {
     this.aWeek = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
     return this.aWeek[a]
 }
-function openCalendar(opts) {
-    var dateForTips=(typeof SERVERTIME !== "undefined") ? new Date(SERVERTIME) : new Date();    
-
-    var _tempDate = new Date(dateForTips.getFullYear(), dateForTips.getMonth(),dateForTips.getDate() + 59);
-
-    var _tempfutureDate=new Date(_tempDate.getTime()+6 * 24 * 3600 * 1000);
-    var monthEnd=(_tempDate.getMonth() + 1);
-    var dayEnd=_tempDate.getDate();
-    var futureMothEnd=(_tempfutureDate.getMonth() + 1);
-    var futureDayEnd=_tempfutureDate.getDate();
-    var isChangeOrder=getRequest().isChangeOrder==1?true:false;
-    var type="";
-    var tipsText="";
-    type=isChangeOrder?"":"抢";
-    //判断12月30日前后
-    var judgeDate = new Date("2016/12/30").getTime();
-    var nowDate = new Date().getTime();
-    if(nowDate>=judgeDate){
-        monthEnd = new Date(nowDate+29*24*60*60*1000).getMonth()+1;
-        dayEnd = new Date(nowDate+29*24*60*60*1000).getDate();
-        futureMothEnd = new Date(nowDate+75*24*60*60*1000).getMonth()+1;
-        futureDayEnd = new Date(nowDate+75*24*60*60*1000).getDate();
-    }else{
-        monthEnd = new Date(nowDate+59*24*60*60*1000).getMonth()+1;
-        dayEnd = new Date(nowDate+59*24*60*60*1000).getDate();
-        futureMothEnd = new Date(nowDate+75*24*60*60*1000).getMonth()+1;
-        futureDayEnd = new Date(nowDate+75*24*60*60*1000).getDate();
-    }
-    tipsText=isChangeOrder?"":"<br />可提前预约"+futureMothEnd+"月"+futureDayEnd+"日前的火车票，开售自动为您抢票</p>";
-    var calendar=new Calendar({
-        tips:{
-            //text:"<p class='buyDate'>今天是{$month}月{$day}号，可购买"+monthEnd+"月"+dayEnd+"日的火车票"+tipsText
-            text:"<p class = 'buyDate'>因铁路局列车运行图调整，火车票预售期调整为30天，建议您提前预约抢票，开售自动抢票。</p>"
-        },
-        type: type,
-        count: opts.count || 4,
-        days: opts.days || 59,
-        dely: 250,
-        date: opts.todayDate || ((typeof SERVERTIME !== "undefined") ? new Date(SERVERTIME) : new Date()),
-        select: opts.select,
-        range: opts.range || [],
-        onCreate: function (dom) {
-            new List({
-                floatContainer: document.querySelector(".float-header"),
-                Scroll: (function () {
-                    var listeners = [];
-                    var top;
-                    var scroll = new iScroll("calendars", {
-                        onScrollMove: function (e) {
-                            top = -this.y;
-                            listeners.forEach(function (fn) {
-                                fn.call(window);
-                            });
-                        },
-                        onScrollEnd: function () {
-                            if (document.querySelector(".float-header")) {
-                                document.querySelector(".float-header").style.opacity = 1;
-                            }
-                        }
-                    });
-                    return {
-                        onscroll: function (fn) {
-                            listeners.push(fn);
-                        },
-                        getScrollTop: function () {
-                            return top;
-                        },
-                        getOffsetTop: function (target) {
-                            return target.offsetTop;
-                        }
-                    }
-                })(),
-                targets: document.querySelectorAll(".calendars-wrapper h3")
-            });
-            function setHoliday(dataStr){
-                if ($($(dom).find("a")[i]).attr('data-day') == dataStr) {
-                    $($(dom).find("a")[i]).find("div:first-child").html('<div class="tag_rest"><span class="tag_content">假</span></div>'+zj.date.parse(dataStr).getDate());
-                }
-            }
-            // 对部分法定假日提前放假的情况 调整
-            for (var i = 0; i < $(dom).find("a").length; i++) {
-                setHoliday("2016-4-30");
-                setHoliday("2016-4-2");
-                setHoliday("2016-4-3");
-            }
-            if (opts.onCreate) opts.onCreate();
-        },
-        canChange: function (date, el) {
-            if ($(el).find('div').hasClass('disabled') || $(el).attr('data-day') == (null || "")) {
-                /* 给可售期后的添加事件 */
-                var p = $(el).attr('data-day');
-                var policydate = new Date(zj.date.parse(p).setDate(zj.date.parse(p).getDate() - 59));
-                var todayDate = (typeof SERVERTIME !== "undefined") ? new Date(SERVERTIME) : new Date();
-                var appointDate=new Date((new Date(date.replace(/-/g, "/"))).getTime()-64*24*3600*1000);
-                if (policydate.getTime() > todayDate.getTime()) {
-                    var subMsg="您可在"+appointDate.format('yyyy年MM月dd日')+"提前预约抢票。";
-                    if(isChangeOrder){
-                        subMsg="";
-                    }
-                    mobileUtil.dialog("预售期为60天，您所选日期将在" + policydate.format('yyyy年MM月dd日') + "开售。"+subMsg, "body");
-                }
-                return false;
-            }
-            $(el).parent().parent().parent().find('a').find('div').removeClass('nian_select');
-            return true;
-        },
-        onChange: function (value) {
-            if (opts.onChange) opts.onChange(value);
-        }
-    });
-}
-
-
   /**
    * 生成一个唯一的guid
    * @return {string}
