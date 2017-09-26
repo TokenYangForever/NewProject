262a78edaa72a7ec9a607bd3a4ca55efe4d9caf7
tangyangyang <tangyang22007@126.com>
2017-09-25 23:05:25 -0500

    Update wxCommon.js

--- wxCommon.js
+++ wxCommon.js
@@ -526,3 +526,14 @@ function isNative (Ctor) {
       return result;
     };
   };
+function looseEqual (a, b) {
+  var isObjectA = isObject(a);
+  var isObjectB = isObject(b);
+  if (isObjectA && isObjectB) {
+    return JSON.stringify(a) === JSON.stringify(b)
+  } else if (!isObjectA && !isObjectB) {
+    return String(a) === String(b)
+  } else {
+    return false
+  }
+}
